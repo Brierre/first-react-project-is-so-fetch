1ff063769d1f7cda04bacd5495764cc92abd0e10
Brierre <hlmbugle@gmail.com>
2023-06-25 20:18:17 -0400

    image edit

--- src/MeanImage.js
+++ src/MeanImage.js
@@ -4,7 +4,7 @@ import meanimage from './img/fuNmGKEf99RWgwVWj24sdb-970-80.jpg';
 export default class MeanImage extends Component {
     render() {
         return (
-            <img src={meanimage} width="45%" alt="meangirls"/>
+            <img src={meanimage} alt="meangirls"/>
         )
     }
 };
